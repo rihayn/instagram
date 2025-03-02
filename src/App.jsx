1b766a740f6c56c5f3ae3d133199eb8fd7f9a025
hiva <hiva.codes@gmail.com>
2025-03-02 09:57:57 +0330

    Implement layout UI and route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignUp from "./components/auth/signUp";
 import Login from "./components/auth/login";
+import Home from "./components/home";
+import Profile from "./components/profile";
+import Layout from "./components/layout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/feed" element={<Layout />}>
+        <Route path="/feed/home" element={<Home />} />
+        <Route path="/feed/profile" element={<Profile />} />
+      </Route>
     </Routes>
   );
 }
