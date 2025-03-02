c4715641756dbcddd1bef5217c03b122818c249d
sjad002 <sajjadizady77@gmail.coim>
2025-03-02 13:07:48 +0000

    profile api

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,16 +1,29 @@
-import Profilepic from "../assets/Frame.svg"
+import { useState } from "react"
+import { client } from "../../lib/axios";
 export default function Profile() {
+
+    const [userData, setUserData] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchUserData = (usersData) => {
+        try {
+            const response = client.get(`api/user/u/${usersData}`)
+            setUserData(response.data);
+        } catch (error) {
+            setError(error.message)
+        }
+    }
     return (
         <div className="w-4/6 h-[450px] flex px-20 gap-[5vw] border-b-1 border-indigo-500" >
             <div className=" h-48 flex justify-center items-center pl-20 ">
                 <div className="w-24 h-24 flex  ">
-                    <img src={Profilepic} alt="" className="rounded-full" />
+                    <img src={userData.profilePicture} alt="" className="rounded-full" />
                 </div>
             </div>
             <div>
                 <div className=" flex gap-[2vw] pt-12 ">
-                    <h1 className="text-xl font-bold">
-                        sajjad
+                    <h1 className="text-xl font-bold text-[#262626]">
+                        {userData.username}
                     </h1>
                     <button className="w-[117px] h-[32px] bg-[#0095F6] rounded-xl cursor-pointer">
                         Following
@@ -18,29 +31,29 @@ export default function Profile() {
                 </div>
                 <div className="flex gap-[25px]">
                     <div className="flex pt-6 gap-[10px]">
-                        <h1 className="font-bold">
-                            1882
+                        <h1 className="font-bold text-[#262626]">
+                            1886
                         </h1>
-                        <p>posts</p>
+                        <p className="text-[#262626]">posts</p>
                     </div>
                     <div className="flex pt-6 gap-[10px]">
-                        <h1 className="font-bold">
-                            4M
+                        <h1 className="font-bold text-[#262626]">
+                            {userData.followers}
                         </h1>
-                        <p>followers</p>
+                        <p className="text-[#262626]">followers</p>
                     </div>
                     <div className="flex pt-6 gap-[10px]">
-                        <h1 className="font-bold">
-                            454
+                        <h1 className="font-bold text-[#262626]">
+                            {userData.followings}
                         </h1>
-                        <p>following</p>
+                        <p className="text-[#262626]">following</p>
                     </div>
                 </div>
                 <div className="flex flex-col  pt-[20px]">
-                    <h1>
+                    <h1 className="text-[#262626]">
                         Marques Brownlee
                     </h1>
-                    <p>
+                    <p className="text-[#262626]">
                         I promise I won't overdo the filters.
                     </p>
                     <a href="" className="text-[#00376B]">mkbhd.com</a>
